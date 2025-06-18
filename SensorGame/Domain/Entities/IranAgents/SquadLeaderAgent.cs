d3dd26096e157099c4173009a1940d259d3d0a47
eli_shteinman <a0533057932@gmail.com>
2025-06-18 22:41:25 +0300

    Implement investigation logic for `SquadLeaderAgent` and `SeniorCommanderAgent`, including sensor activation, weakness handling, and periodic sensor removal.

--- SensorGame/Domain/Entities/IranAgents/SquadLeaderAgent.cs
+++ SensorGame/Domain/Entities/IranAgents/SquadLeaderAgent.cs
@@ -4,6 +4,12 @@ namespace SensorGame.Domain.Entities.IranAgents;
 
 public class SquadLeaderAgent : IranAgent
 {
+	private readonly Random Random = new();
+	private int CounterOfInvestigations = 0;
+	private void Remove1Sensor()
+	{
+		AttachedSensors[Random.Next(0, AttachedSensors.Length)] = null;
+	}
 	public SquadLeaderAgent()
 			: base(WeaknessesFactory.CreateRandomWeakness(4))
 	{
@@ -12,6 +18,39 @@ public class SquadLeaderAgent : IranAgent
 
 	protected override InvestigationAggregateResult Investigate()
 	{
-		throw new NotImplementedException();
+		CounterOfInvestigations++;
+		if (CounterOfInvestigations % 3 == 0)
+		{
+			Remove1Sensor();
+		}
+		var brokenCount = 0;
+		var results = new List<SensorActiveResult>();
+		var copyWeaknesses = Weaknesses.ToList();
+		foreach (var sensor in AttachedSensors)
+		{
+			if (sensor == null)
+			{
+				continue;
+			}
+			var result = sensor.Activate();
+			if (result.WasBroken)
+			{
+				brokenCount++;
+			}
+			else
+			{
+				copyWeaknesses.Remove(result.Type);
+			} 
+			results.Add(result);
+		}
+		IsExposed = (copyWeaknesses.Count == 0);
+		return new InvestigationAggregateResult
+		{
+			AgentRank = Rank,
+			CorrectMatches = (Weaknesses.Length - copyWeaknesses.Count),
+			TotalWeaknesses = Weaknesses.Length,
+			InternalSensorResults = results,
+			BrokenCount = brokenCount
+		};
 	}
 }
